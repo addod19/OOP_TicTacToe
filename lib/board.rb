a95d1c62fdbe484bee8a2f17a44187af37d46917
maya88en <maya88en@gmail.com>
2019-09-12 13:49:31 +0200

    updated board display

--- lib/board.rb
+++ lib/board.rb
@@ -5,15 +5,10 @@ class Board
         @grid = [1,2,3,4,5,6,7,8,9]
     end
     def display
-        print "/t|-------------|"
-        print "/t|Welcome to TIC TAC TOE|"
-        print "/t|------------|"
-        print "/t #{grid[0]}|#{grid[1]}|#{grid[2]}"
-        print "/t |------|----------|"
-        print "/t #{grid[3]}|#{grid[4]}| #{grid[5]}"
-        print "/t |------|----------|"
-        print "/t #{grid[6]}| #{grid[7]}| #{grid[8]}"
-        print "/t |------|----------|"
+        puts "|Welcome to TIC TAC TOE|"
+        puts " #{grid[0]}|#{grid[1]}|#{grid[2]}"
+        puts " #{grid[3]}|#{grid[4]}|#{grid[5]}"
+        puts " #{grid[6]}|#{grid[7]}|#{grid[8]}"
     end
 end 
 
