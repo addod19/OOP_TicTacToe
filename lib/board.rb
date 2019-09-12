c325000547143e2f5d386d87fb496b47cd2544de
addod19 <addodaniellarbi@gmail.com>
2019-09-12 12:41:02 +0000

    added some puts statement the make the console friendly

--- lib/board.rb
+++ lib/board.rb
@@ -5,10 +5,17 @@ class Board
         @grid = [1,2,3,4,5,6,7,8,9]
     end
     def display
-        puts "|Welcome to TIC TAC TOE|"
-        puts " #{grid[0]}|#{grid[1]}|#{grid[2]}"
-        puts " #{grid[3]}|#{grid[4]}|#{grid[5]}"
-        puts " #{grid[6]}|#{grid[7]}|#{grid[8]}"
+        puts "       | Welcome to TIC TAC TOE GAME |"
+        puts ""
+        puts "Player1 uses the symbol 'X' and Player2 uses 'O', players take turns, a player cannot visit a cell already visited "
+        puts "A Player wins if he place the Symbol in the order  => [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]"
+        puts ""
+        puts "             #{grid[0]}  |    #{grid[1]}  |  #{grid[2]}"
+        puts "            ----- ----- -----"
+        puts "             #{grid[3]}  |    #{grid[4]}  |  #{grid[5]}"
+        puts "            ----- ----- -----"
+        puts "             #{grid[6]}  |    #{grid[7]}  |  #{grid[8]}"
+        
     end
 end 
 
