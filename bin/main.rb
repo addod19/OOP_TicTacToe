6bb6ba8b42257bb70a82a2c14328fa348d948608
addod19 <addodaniellarbi@gmail.com>
2019-09-17 08:36:40 +0000

    resolving comments from code review

--- bin/main.rb
+++ bin/main.rb
@@ -2,23 +2,35 @@ require_relative '../lib/game'
 require_relative '../lib/player'
 require_relative '../lib/board'
 
-puts "Welcome to Tic Tac Toe Game"
-
-puts "This game is played by 2 players, player1 uses 'X' and player2 uses 'O'"
+board = Board.new
 
 puts "Enter name for player 1"
 player1 = gets.strip.capitalize
-player1_id = "X"
+puts "Choose a marker [X or O]"
+marker1 = gets.chomp.upcase
+
 
 puts "Enter name for player 2"
 player2 = gets.strip.capitalize
-player2_id = "O"
+puts "Choose a marker [X or O]"
+marker2 = gets.chomp.upcase
 
-board = Board.new
+if marker1 == "X" && marker2 == "O"
+  puts "#{player1} goes with marker #{marker1}"
+  puts "#{player2} goes with marker #{marker2}"
+elsif marker1 == "O" && marker2 == "X"
+  puts "#{player1} goes with marker #{marker1}"
+  puts "#{player2} goes with marker #{marker2}"
+else
+  puts "Enter the right marker!"
+end
 
-Game.new(player1, player2, board).
+
+Game.new(player1, player2, board).game_start
 
 puts "Would like to play again? [y/ n]"
+
+
 
 
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,61 @@
 # frozen_string_literal: true
+require 'set'
 
 class Game
-  def game_start; end
+  @@Wins = [ [1,2,3], [1,4,7], [1,5,7], [2,5,8], [3,6,9], [3,5,7], [4,5,6], [7,8,9] ]
+  @@options = []
+  
+  def initialize(player1, player2, board)
+    @player1 = player1
+    @player2 = player2
+    @board = board
+  end
+  def game_start 
+
+    a = 0
+
+    until board_full?()
+      @board.display
+      option = 0
+
+      current_player = a.even? ? @player1 : @player2
+
+      loop do
+        puts " #{current_player}, select an option from 1..9"
+        option = gets.chomp.to_i
+
+        break if valid_move?(option)
+      end
+
+      char = (a.even? ? "X": "O").to_s
+      @board.grid[option-1] = char if @board.grid.include?(option)
+
+      @@Wins = @@Wins.each do |moves|
+        moves[moves.find_index(option)] = char if moves.include?(option)
+        if Set.new(moves).length == 0
+          puts "Game Over, #{current_player} wins!"
+          board.display
+          return
+        end
+      end
+      @@options.push(option)
+      a += 1
+    end
+    puts "Board FUll, the Game is a tie (draw)"
+  end
+
+  def board_full?
+    return false if @board.grid.any? { |cell| cell.is_a?(Integer) }
+    true
+  end
+
+  def valid_move?(user_input)
+    unless user_input.is_a?(Integer) && ((user_input <= 9) && (user_input.positive?)) && !@@options.include?(user_input)
+      puts 'Wrong Input, Enter 1..9'
+      return false
+
+    end
+    true
+  end
 end
+
