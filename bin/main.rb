e15f0119684ddfbb26ccc1cdd663abcd58e7e12c
maya88en <maya88en@gmail.com>
2019-09-17 15:28:09 +0200

    oved p,puts, print to the main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,30 @@ require_relative '../lib/game'
 require_relative '../lib/player'
 require_relative '../lib/board'
 
+# frozen_string_literal: true
+
+class Board
+  attr_accessor :grid
+
+  def initialize
+    @grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def display
+    puts 'Welcome to Tic Tac Toe Game'
+    puts 'This game is played by 2 players, each player will enter his/her name and choose a marker [X or O]'
+    puts ''
+    puts "#{grid[0]}|#{grid[1]}|#{grid[2]}"
+    puts '----- ----- -----'
+    puts "#{grid[3]}|#{grid[4]}|#{grid[5]}"
+    puts '----- ----- -----'
+    puts "#{grid[6]}|#{grid[7]}|#{grid[8]}"
+  end
+end
+
 board = Board.new
+p board.display
+
 
 puts "Enter name for player 1"
 player1 = gets.strip.capitalize
@@ -29,6 +52,9 @@ end
 Game.new(player1, player2, board).game_start
 
 puts "Would like to play again? [y/ n]"
+
+
+
 
 
 
