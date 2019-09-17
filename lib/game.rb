6e59824d0d95eaa693137a7962be63395fccc304
addod19 <addodaniellarbi@gmail.com>
2019-09-17 08:42:50 +0000

    lint error checks

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
+
 require 'set'
 
 class Game
-  @@Wins = [ [1,2,3], [1,4,7], [1,5,7], [2,5,8], [3,6,9], [3,5,7], [4,5,6], [7,8,9] ]
+  @@Wins = [[1, 2, 3], [1, 4, 7], [1, 5, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]]
   @@options = []
-  
+
   def initialize(player1, player2, board)
     @player1 = player1
     @player2 = player2
     @board = board
   end
-  def game_start 
 
+  def game_start
     a = 0
 
-    until board_full?()
+    until board_full?
       @board.display
       option = 0
 
@@ -27,30 +28,31 @@ class Game
         break if valid_move?(option)
       end
 
-      char = (a.even? ? "X": "O").to_s
-      @board.grid[option-1] = char if @board.grid.include?(option)
+      char = (a.even? ? 'X' : 'O').to_s
+      @board.grid[option - 1] = char if @board.grid.include?(option)
 
       @@Wins = @@Wins.each do |moves|
         moves[moves.find_index(option)] = char if moves.include?(option)
-        if Set.new(moves).length == 0
-          puts "Game Over, #{current_player} wins!"
-          board.display
-          return
-        end
+        next unless Set.new(moves).empty?
+
+        puts "Game Over, #{current_player} wins!"
+        board.display
+        return
       end
       @@options.push(option)
       a += 1
     end
-    puts "Board FUll, the Game is a tie (draw)"
+    puts 'Board FUll, the Game is a tie (draw)'
   end
 
   def board_full?
     return false if @board.grid.any? { |cell| cell.is_a?(Integer) }
+
     true
   end
 
   def valid_move?(user_input)
-    unless user_input.is_a?(Integer) && ((user_input <= 9) && (user_input.positive?)) && !@@options.include?(user_input)
+    unless user_input.is_a?(Integer) && ((user_input <= 9) && user_input.positive?) && !@@options.include?(user_input)
       puts 'Wrong Input, Enter 1..9'
       return false
 
@@ -58,4 +60,3 @@ class Game
     true
   end
 end
-
