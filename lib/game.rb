36316d67b122e8695a0b125b352d1bccda080a0c
addod19 <addodaniellarbi@gmail.com>
2019-09-20 10:46:49 +0000

    removed attr_acessor from the game class, added player1,player2 to attr_reader

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Game
-  attr_accessor :player1, :player2
-  attr_reader :current_player, :board
+  attr_reader :player1, :player2, :current_player, :board
 
   def initialize
     @board = Board.new
